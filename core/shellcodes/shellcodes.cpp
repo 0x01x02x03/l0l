a556e2880c3d463d7d56f22a63c4733881379ef5
roissy <roissy@tuta.io>
2016-08-30 01:40:28 +0300

    Shellcode output type developed

--- core/shellcodes/shellcodes.cpp
+++ core/shellcodes/shellcodes.cpp
@@ -41,13 +41,35 @@ void genshellcode(int argp, string os, string type, string a1, string a2)
 
     }
 
-    session += "print \"\"\"\nchar shellcode [] = \"%s\";\"\"\"\ % shellcode\n";
+        session +=
+                    "lenlen = len(shellcode)\n"
+                    "print \"Shellcode lenght: %s\" % lenlen\n"
+                    "print \"\"\n";
+
+        session +=
+                    "b = shellcode.split(\"\\\\\")\n"
+                    "x = 1\n"
+                    "p = (len(b)/15)+1\n"
+                    "loplop = 1\n"
+                    "for i in range((len(b)/15)+1):\n"
+                        "\tj = b[x:x+15]\n"
+                        "\tline = \"\"\n"
+                        "\tfor k in range(len(j)):\n"
+                            "\t\tline += \"\\\\\"+j[k]\n"
+                        "\tif p != loplop:\n"
+                            "\t\tprint '\t\"'+line+'\"+'\n"
+                        "\telse:\n"
+                            "\t\tprint '\t\"'+line+'\";'\n"
+                        "\tx += 10\n"
+                        "\tloplop += 1";
+
 
     char filename[] = "session.py";
     std::ofstream outfile (filename);
     outfile << session;
     outfile.close();
 
+    cout << "\n";
 
     #ifdef OS_Windows
         system("python session.py");
